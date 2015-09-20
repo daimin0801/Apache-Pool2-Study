f0482e4d5015a255690e7b8d9ae4a07339fd9c2a
daimin0801 <daimin0801@163.com>
2015-09-20 22:18:16 +0800

    加状态注释

--- ApachPoolNew/src/main/java/enums/PooledObjectState.java
+++ ApachPoolNew/src/main/java/enums/PooledObjectState.java
@@ -1,58 +1,56 @@
 package enums;
 
 public enum PooledObjectState {
+	/**
+	 * 位于队列中，未使用
+	 */
     IDLE,
-    ALLOCATED,
-
+    
     /**
-     * In the queue, currently being tested for possible eviction.
+     * 在使用
+     */
+    ALLOCATED,
+ 
+    /**
+     * 位于队列中，当前正在测试，可能会被回收
      */
     EVICTION,
 
     /**
-     * Not in the queue, currently being tested for possible eviction. An
-     * attempt to borrow the object was made while being tested which removed it
-     * from the queue. It should be returned to the head of the queue once
-     * eviction testing completes.
-     * TODO: Consider allocating object and ignoring the result of the eviction
-     *       test.
+     * 不在队列中，当前正在测试，可能会被回收。从池中借出对象时需要从队列出移除并进行测试
      */
     EVICTION_RETURN_TO_HEAD,
 
     /**
-     * In the queue, currently being validated.
+     * 位于队列中，当前正在验证
      */
     VALIDATION,
 
     /**
-     * Not in queue, currently being validated. The object was borrowed while
-     * being validated and since testOnBorrow was configured, it was removed
-     * from the queue and pre-allocated. It should be allocated once validation
-     * completes.
+     * 不在队列中，当前正在验证。当对象从池中被借出，
+     * 在配置了testOnBorrow的情况下，对像从队列移除和进行预分配的时候会进行验证
      */
     VALIDATION_PREALLOCATED,
 
     /**
-     * Not in queue, currently being validated. An attempt to borrow the object
-     * was made while previously being tested for eviction which removed it from
-     * the queue. It should be returned to the head of the queue once validation
-     * completes.
+     * 不在队列中，正在进行验证。
+     * 从池中借出对象时，从队列移除对象时会先进行测试。
+     * 返回到队列头部的时候应该做一次完整的验证
      */
     VALIDATION_RETURN_TO_HEAD,
 
     /**
-     * Failed maintenance (e.g. eviction test or validation) and will be / has
-     * been destroyed
+     * 回收或验证失败，将销毁
      */
     INVALID,
 
     /**
-     * Deemed abandoned, to be invalidated.
+     * 即将无效
      */
     ABANDONED,
 
     /**
-     * Returning to the pool.
+     * 返还到池中
      */
     RETURNING
 }
